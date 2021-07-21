6a61f23f1500d616c18afd08161e77b8d31bf1ff
xasopheno <danny@xasopheno.com>
2021-07-20 22:43:04 -0400

    generates random art

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod state;
 mod vertex;
 use crate::state::State;
 use crate::vertex::Vertex;
+use rand::prelude::*;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -15,32 +16,53 @@ fn main() {
     let window = WindowBuilder::new()
         .build(&event_loop)
         .expect("Unable to create window");
-    const VERTICES: &[Vertex] = &[
-        Vertex {
-            position: [-0.0868241, 0.49240386, 0.0],
-            color: [0.2, 0.0, 0.5],
-        },
-        Vertex {
-            position: [-0.49513406, 0.06958647, 0.0],
-            color: [0.5, 1.0, 0.5],
-        },
-        Vertex {
-            position: [-0.21918549, -0.44939706, 0.0],
-            color: [0.1, 0.0, 0.8],
-        },
-        Vertex {
-            position: [0.35966998, -0.3473291, 0.0],
-            color: [0.8, 0.0, 0.2],
-        },
-        Vertex {
-            position: [0.44147372, 0.2347359, 0.0],
-            color: [0.5, 0.4, 0.2],
-        },
-    ];
+    // const VERTICES: &[Vertex] = &[
+    // Vertex {
+    // position: [-0.0868241, 0.49240386, 0.0],
+    // color: [0.2, 0.0, 0.5],
+    // },
+    // Vertex {
+    // position: [-0.49513406, 0.06958647, 0.0],
+    // color: [0.5, 1.0, 0.5],
+    // },
+    // Vertex {
+    // position: [-0.21918549, -0.44939706, 0.0],
+    // color: [0.1, 0.0, 0.8],
+    // },
+    // Vertex {
+    // position: [0.35966998, -0.3473291, 0.0],
+    // color: [0.8, 0.0, 0.2],
+    // },
+    // Vertex {
+    // position: [0.44147372, 0.2347359, 0.0],
+    // color: [0.5, 0.4, 0.2],
+    // },
+    // Vertex {
+    // position: [0.7, 0.8, 0.0],
+    // color: [0.1, 0.2, 0.1],
+    // },
+    // ];
+    let mut rng = rand::thread_rng();
 
-    const INDICES: &[u16] = &[0, 1, 4, 3, 2, 4, 2, 1, 4, 0];
+    let mut r = || rng.gen::<f32>() * 2.0 - 1.0;
 
-    let mut state = block_on(State::new(&window, VERTICES, INDICES));
+    let vertices: Vec<Vertex> = (0..8)
+        .map(|_| Vertex {
+            position: [r(), r(), r()],
+            color: [r(), r(), r()],
+        })
+        .collect();
+
+    let mut num = || rng.gen_range(0..vertices.len() as u16);
+
+    // let INDICES: &[u16] = &[0, 1, 4, 3, 6, 2, 4, 2, 1, 4, 5, 0];
+    let indices: Vec<u16> = (0..100).map(|_| num()).collect();
+
+    let mut state = block_on(State::new(
+        &window,
+        &vertices.as_slice(),
+        &indices.as_slice(),
+    ));
 
     event_loop.run(move |event, _, control_flow| match event {
         Event::RedrawRequested(_) => {
