df81fa5e8ae73914983b927dce770ea3b5b7e929
xasopheno <danny@xasopheno.com>
2021-09-15 21:59:15 -0400

    wip

--- egui_demo_lib/src/apps/demo/widget_gallery.rs
+++ egui_demo_lib/src/apps/demo/widget_gallery.rs
@@ -12,14 +12,14 @@ impl Default for WidgetGallery {
 
 impl super::Demo for WidgetGallery {
     fn name(&self) -> &'static str {
-        "WereSoCool: Kintaro"
+        "Kintaro"
     }
 
     fn show(&mut self, ctx: &egui::CtxRef, open: &mut bool) {
         egui::Window::new(self.name())
             .open(open)
             .resizable(true)
-            .default_width(300.0)
+            .default_width(200.0)
             .show(ctx, |ui| {
                 use super::View;
                 self.ui(ui);
@@ -30,16 +30,14 @@ impl super::Demo for WidgetGallery {
 impl super::View for WidgetGallery {
     fn ui(&mut self, ui: &mut egui::Ui) {
         ui.scope(|ui| {
-            egui::Grid::new("my_grid")
+            egui::Grid::new("Kintaro")
                 .num_columns(2)
-                .spacing([40.0, 4.0])
+                .spacing([20.0, 2.0])
                 .striped(true)
                 .show(ui, |ui| {
                     self.gallery_grid_contents(ui);
                 });
         });
-
-        ui.separator();
     }
 }
 
@@ -47,27 +45,16 @@ impl WidgetGallery {
     fn gallery_grid_contents(&mut self, ui: &mut egui::Ui) {
         let Self { boolean, .. } = self;
 
-        ui.add(doc_link_label("Camera", "button"));
-        if ui.button("Camera1").clicked() {
+        ui.label("Camera");
+        if ui.button("A").clicked() {
             *boolean = !*boolean;
         }
-        // ui.add(doc_link_label("CameraButton2", "button"));
-        if ui.button("Camera2").clicked() {
+        if ui.button("B").clicked() {
+            *boolean = !*boolean;
+        }
+        if ui.button("C").clicked() {
             *boolean = !*boolean;
         }
         ui.end_row();
-    }
-}
-
-fn doc_link_label<'a>(title: &'a str, search_term: &'a str) -> impl egui::Widget + 'a {
-    let label = format!("{}:", title);
-    let url = format!("https://docs.rs/egui?search={}", search_term);
-    move |ui: &mut egui::Ui| {
-        ui.hyperlink_to(label, url).on_hover_ui(|ui| {
-            ui.horizontal_wrapped(|ui| {
-                ui.label("Search egui docs for");
-                ui.code(search_term);
-            });
-        })
     }
 }
