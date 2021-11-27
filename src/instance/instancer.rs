90347bfe601439ee48879a63e21c35d171e2b462
xasopheno <danny@xasopheno.com>
2021-11-26 19:22:12 -0500

    WIP

--- src/instance/instancer.rs
+++ src/instance/instancer.rs
@@ -21,55 +21,65 @@ pub trait Instancer: dyn_clone::DynClone + Debug {
 }
 dyn_clone::clone_trait_object!(Instancer);
 
+pub struct Op4DToInstanceInput<'a> {
+    canvas: &'a Canvas,
+    x: f32,
+    y: f32,
+    z: f32,
+    length: f32,
+    life: f32,
+    size: f32,
+}
+
 impl Instancer for SimpleInstancer {
     fn update_instance(&self, instance: &mut Instance, dt: f32) {
         instance.life -= dt * 0.1;
-        // instance.position.y += f32::sin(3.0 * (2.0 - instance.life));
         instance.position.x += 800.0 * (2.0 - instance.life) * f32::signum(instance.position.x);
+        // instance.position.y += f32::sin(3.0 * (2.0 - instance.life));
         // instance.position.y += 700.0 * (2.0 - instance.life) * f32::signum(instance.position.y);
         // f32::sin(dt * 0.1 * f32::sin(instance.position.x / instance.position.y) * f32::tan(instance.life));
     }
 
-    fn op4d_to_instance(
-        &self,
-        instance_mul: &InstanceMul,
-        op4d: &Op4D,
-        canvas: &Canvas,
-    ) -> Instance {
-        let mut rng = rand::thread_rng();
-        let rotation = cgmath::Quaternion::from_axis_angle(
-            cgmath::Vector3::unit_x(),
-            // cgmath::Deg(0.0),
-            cgmath::Deg(rng.gen_range(-0.3..0.3)),
-        );
-        let n_row = canvas.n_row;
-        let n_column = canvas.n_column;
-        let x = -op4d.x as f32 * instance_mul.x;
-        let y = op4d.y as f32 * instance_mul.y;
-        let z = op4d.z as f32 * instance_mul.z;
-        let length = op4d.l as f32 * instance_mul.length;
-        let life = 1.0 * instance_mul.life;
-        let size = instance_mul.size * f32::max(z, 0.2);
-        Instance {
-            position: Vector3::new(
-                // n_row as f32 * 1.0 / 2.0 * f32::powi(x, 2),
-                n_row as f32 * x,
-                n_column as f32 * y,
-                // n_row as f32 * x / 3.0 * y,
-                // sin(x ^ 1.0 / 2.0 - y ^ -2.0),
-                // n_row as f32 * 1.0 * y / x,
-                // n_row as f32 * (op4d.x * op4d.x) as f32 * 2.0 / op4d.y as f32,
-                // n_column as f32 * (op4d.y * op4d.y) as f32 / 2.0 * 10.0,
-                // (op4d.x as f32 * n_row as f32 * 4.0 / op4d.y as f32) / 3.0,
-                // * f32::sin(1000.0 * op4d.y as f32),
-                // op4d.y as f32 * n_column as f32 * 9.0,
-                1.0,
-            ) - canvas.instance_displacement,
-            rotation,
-            life,
-            size,
-            length,
-            names: op4d.names.to_owned(),
-        }
+    fn op4d_to_instance() -> Instance {}
+}
+
+fn op4d_to_instance_first(
+    instancer: &dyn Instancer,
+    instance_mul: &InstanceMul,
+    op4d: &Op4D,
+    canvas: &Canvas,
+) -> Op4DToInstanceInput {
+    let mut rng = rand::thread_rng();
+    let rotation = cgmath::Quaternion::from_axis_angle(
+        cgmath::Vector3::unit_x(),
+        cgmath::Deg(rng.gen_range(-0.3..0.3)),
+    );
+    let n_row = canvas.n_row;
+    let n_column = canvas.n_column;
+    let x = -op4d.x as f32 * instance_mul.x;
+    let y = op4d.y as f32 * instance_mul.y;
+    let z = op4d.z as f32 * instance_mul.z;
+    let length = op4d.l as f32 * instance_mul.length;
+    let life = 1.0 * instance_mul.life;
+    let size = instance_mul.size * f32::max(z, 0.2);
+
+    Instance {
+        position: Vector3::new(n_row as f32 * x, n_column as f32 * y, 1.0)
+            - canvas.instance_displacement,
+        rotation,
+        life,
+        size,
+        length,
+        names: op4d.names.to_owned(),
     }
 }
+
+// n_row as f32 * 1.0 / 2.0 * f32::powi(x, 2),
+// n_row as f32 * x / 3.0 * y,
+// sin(x ^ 1.0 / 2.0 - y ^ -2.0),
+// n_row as f32 * 1.0 * y / x,
+// n_row as f32 * (op4d.x * op4d.x) as f32 * 2.0 / op4d.y as f32,
+// n_column as f32 * (op4d.y * op4d.y) as f32 / 2.0 * 10.0,
+// (op4d.x as f32 * n_row as f32 * 4.0 / op4d.y as f32) / 3.0,
+// * f32::sin(1000.0 * op4d.y as f32),
+// op4d.y as f32 * n_column as f32 * 9.0,
